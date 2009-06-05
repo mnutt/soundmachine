e01464153640bbcf189427dbb061b0e6caa4383f
Michael Nutt <michael@nuttnet.net>
2009-06-05 19:54:54 -0400

    Remove the table

--- soundmachine.php
+++ soundmachine.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: SoundMachine
 Plugin URI: http://blog.limewire.com
-Description: An mp3 player widget based on the excellent SoundManager2.  Add links in wp-admin and they show up as a fully-customizable html mp3 player.  Loosely adapted from Flash MP3 Widget by Charles Tang (email : charlestang@foxmail.com).
+Description: An mp3 player widget based on the excellent SoundManager2.  Add links in wp-admin and they show up as a fully-customizable html mp3 player.  Loosely adapted from Flash MP3 Widget by Charles Tang.
 Version: 0.2
 Author: Michael Nutt
 Author URI: http://nuttnet.net
@@ -129,31 +129,31 @@ function soundmachine_init() {
   function soundmachine_options() {
     $options = get_option('soundmachine');
     if (!is_array($options)) {
-      $options = array('title' => __('Flash MP3 Player','fmp'), 'width' => 177, 'height' => 280);
+      $options = array('title' => __('SoundMachine','sm'), 'width' => 177, 'height' => 280);
     }
-    if ($_POST['fmp-submit']) {
-      $options['title'] = strip_tags($_POST['fmp-title']);
-      $options['width'] = intval($_POST['fmp-width']);
-      $options['height'] = intval($_POST['fmp-height']);
+    if ($_POST['sm-submit']) {
+      $options['title'] = strip_tags($_POST['sm-title']);
+      $options['width'] = intval($_POST['sm-width']);
+      $options['height'] = intval($_POST['sm-height']);
       update_option('soundmachine', $options);
     }
-    echo '<p style="text-align: left;"><label for="fmp-title">';
-    _e('Title: ','fmp');
-    echo '</label><input type="text" id="fmp-title" name="fmp-title" value="'.htmlspecialchars(stripslashes($options['title'])).'" /></p>'."\n";
-    echo '<p style="text-align: left;"><label for="fmp-width">';
-    _e('Width: ','fmp');
-    echo '</label><input type="text" id="fmp-width" name="fmp-width" value="'.intval($options['width']).'" size="3" /></p>'."\n";
-    echo '<p style="text-align: left;"><label for="fmp-height">';
-    _e('Height: ','fmp');
-    echo '</label><input type="text" id="fmp-height" name="fmp-height" value="'.intval($options['height']).'" size="3" /></p>'."\n";
-    echo '<input type="hidden" id="fmp-submit" name="fmp-submit" value="1" />'."\n";
+    echo '<p style="text-align: left;"><label for="sm-title">';
+    _e('Title: ','sm');
+    echo '</label><input type="text" id="sm-title" name="sm-title" value="'.htmlspecialchars(stripslashes($options['title'])).'" /></p>'."\n";
+    echo '<p style="text-align: left;"><label for="sm-width">';
+    _e('Width: ','sm');
+    echo '</label><input type="text" id="sm-width" name="sm-width" value="'.intval($options['width']).'" size="3" /></p>'."\n";
+    echo '<p style="text-align: left;"><label for="sm-height">';
+    _e('Height: ','sm');
+    echo '</label><input type="text" id="sm-height" name="sm-height" value="'.intval($options['height']).'" size="3" /></p>'."\n";
+    echo '<input type="hidden" id="sm-submit" name="sm-submit" value="1" />'."\n";
   }
         
   $widget_ops =  array('classname' => 'soundmachine', 
-                       'description' => __( 'Flash MP3 player.', 'sm'));
+                       'description' => __( 'SoundMachine.', 'sm'));
   // Register Widgets
-  wp_register_sidebar_widget('soundmachine', __('MP3 Player','sm'), 'soundmachine', $widget_ops);
-  wp_register_widget_control('soundmachine', __('MP3 Player','sm'), 'soundmachine_options', 400, 200);
+  wp_register_sidebar_widget('soundmachine', __('SoundMachine MP3 Player','sm'), 'soundmachine', $widget_ops);
+  wp_register_widget_control('soundmachine', __('SoundMachine MP3 Player','sm'), 'soundmachine_options', 400, 200);
 }
 
 add_action('plugins_loaded','soundmachine_init');
@@ -181,7 +181,7 @@ function sm_initRoles()
 
 function sm_adminMenu() {
   add_options_page( __('SoundMachine', 'sm'), 
-                    __('MP3 list', 'sm'), 
+                    __('MP3 Player', 'sm'), 
                     'admin_sm', 
                     'sm_options', 
                     'sm_pageOptions');
@@ -240,7 +240,7 @@ function sm_pageOptions() {
   
   ?>
   <div class="wrap">
-    <h2><?php _e('MP3 Player','sm'); ?></h2>
+    <h2><?php _e('SoundMachine','sm'); ?></h2>
       <p><?php _e('Visit the <a href="http://www.charlestang.cn/flash-player-widget.htm">plugin\'s homepage</a> for further details. If you find a bug, or have a fantastic idea for this plugin, <a href="mailto:charlestang@foxmail.com">ask me</a> !', 'sm'); ?></p>
       <p><?php 
             _e("Your PHP version is ",'sm'); echo $phpver;
@@ -253,98 +253,82 @@ function sm_pageOptions() {
           ?>
       </p>
 
-      <script src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/soundman-mp3-list/js/sm-admin.js" type="text/javascript"></script>
+      <script src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/soundmachine/js/sm-admin.js" type="text/javascript"></script>
 
-      <link rel="stylesheet" href="<?php echo get_option('siteurl'); ?>/wp-content/plugins/soundman-mp3-list/css/sm-admin.css" type="text/css"></style>
+      <link rel="stylesheet" href="<?php echo get_option('siteurl'); ?>/wp-content/plugins/soundmachine/css/sm-admin.css" type="text/css"></style>
         
-      <form method="post" action="<?php echo $sm_admin_base_url . 'sm_options';?>">
-        <table class="form-table">
-          <tbody>
-            <tr>
-              <th scope="row"><?php _e('Player Properties','sm'); ?></th>
-              <td>
-                <fieldset>
-                  <legend class="hidden"><?php _e('Player Properties','sm'); ?></legend>
-                  <p>
-                    <input type="checkbox" id="sm-show-dispay" name="sm-show-dispay" value="yes" <?php if( $properties['showDisplay'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
-                    <label for="sm-show-dispay"><?php _e('Show upper panel','sm'); ?></label>
-                  </p>
-                  <p>
-                    <input type="checkbox" id="sm-show-playlist" name="sm-show-playlist" value="yes" <?php if( $properties['showPlaylist'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
-                    <label for="sm-show-playlist"><?php _e('Show play list','sm'); ?></label>
-                  </p>
-                  <p>
-                    <input type="checkbox" id="sm-auto-play" name="sm-auto-play" value="yes" <?php if( $properties['autoStart'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
-                    <label for="sm-auto-play"><?php _e('Auto start','sm'); ?></label>
-                  </p>
-                </fieldset>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row"><?php _e('Songs List','sm'); ?></th>
-              <td>
-                <fieldset>
-                  <legend class="hidden"><?php _e('Songs List','sm'); ?></legend>
-                  <p>
-                    <a href="<?php echo $audio_upload_iframe_src; ?>&am;TB_iframe=true" id="add_audio" class="thickbox" title="<?php echo $audio_title; ?>"><img src='images/media-button-music.gif' alt='<?php echo $audio_title; ?>' /> Add Song</a>
-                    &#8212;
-                    <a href="#" onClick="condenseSongs(); return false;">Condense Song List</a>
-                  </p>
+      <form class="form-table" method="post" action="<?php echo $sm_admin_base_url . 'sm_options';?>">
+        <p><?php _e('Player Properties','sm'); ?></p>
+
+        <p>
+          <input type="checkbox" id="sm-show-dispay" name="sm-show-dispay" value="yes" <?php if( $properties['showDisplay'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
+          <label for="sm-show-dispay"><?php _e('Show upper panel','sm'); ?></label>
+        </p>
+        <p>
+          <input type="checkbox" id="sm-show-playlist" name="sm-show-playlist" value="yes" <?php if( $properties['showPlaylist'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
+          <label for="sm-show-playlist"><?php _e('Show play list','sm'); ?></label>
+        </p>
+        <p>
+          <input type="checkbox" id="sm-auto-play" name="sm-auto-play" value="yes" <?php if( $properties['autoStart'] == 'yes' ) echo "checked='checked'"; ?> size="5"/>
+          <label for="sm-auto-play"><?php _e('Auto start','sm'); ?></label>
+        </p>
+
+        <p><?php _e('Songs List','sm'); ?></p>
+        
+	<p>
+          <a href="<?php echo $audio_upload_iframe_src; ?>&am;TB_iframe=true" id="add_audio" class="thickbox" title="<?php echo $audio_title; ?>"><img src='images/media-button-music.gif' alt='<?php echo $audio_title; ?>' /> Add Song</a>
+          &#8212;
+          <a href="#" onClick="condenseSongs(); return false;">Condense Song List</a>
+        </p>
                   
-                  <?php for($i = 0; $i < 15; $i++) { ?>
-                    <?php $song = $songs[$i]; ?>
-                    <div class="song-field">
-                      <div>
-                        <label for="song-title-<?php echo $i;?>">
-                          <?php _e('Title:','sm');?>
-                        </label>
-                        <input class="song-title" type="text" name="song-title-<?php echo $i;?>" id="song-title-<?php echo $i;?>" value="<?php echo stripslashes($song['title']); ?>" size="75"/>
-                      </div>
-                      
-                      <div>
-                        <label for="song-path-<?php echo $i;?>">
-                          <?php _e('URL:','sm');?>
-                        </label>
-                        <input class="song-path" type="text" name="song-path-<?php echo $i;?>" id="song-path-<?php echo $i;?>" value="<?php echo $song['path']; ?>" size="75"/>
-                      </div>
-                      <table class="inline-table">
-                        <tr>
-                          <td>
-                            <div>
-                              <label for="song-buy_link-<?php echo $i;?>">
-                                <?php _e('Buy Link:','sm');?>
-                              </label>
-                              <input class="song-buy_link" type="text" name="song-buy_link-<?php echo $i;?>" id="song-buy_link-<?php echo $i;?>" value="<?php echo $song['buy_link']; ?>" size="75"/>
-                            </div>
-                          </td>
-                          <td>
-                            <div>
-                              <label for="song-album_cover-<?php echo $i;?>">
-                                <?php _e('Album Cover:','sm');?>
-                              </label>
-                              <input class="song-album_cover" type="text" name="song-album_cover-<?php echo $i;?>" id="song-album_cover-<?php echo $i;?>" value="<?php echo $song['album_cover']; ?>" size="75"/>
-                            </div>
-                          </td>
-                          <td class="album_cover_preview"><div class="preview_wrapper"><img src="<?php echo $song['album_cover']; ?>"/></div></td>
-                          <td>
-                            <div class="song-checkbox">
-                              <input class="song-show_download" type="checkbox" name="song-show_download-<?php echo $i;?>" id="song-show_download-<?php echo $i;?>" <?php if($song['show_download'] == true) echo "checked='checked'"; ?> />&nbsp;<label for="song-show-download-<?php echo $i;?>"><?php _e('Show&nbsp;Download','sm');?></label>
-                            </div>
-                          </td>
-                          <td>
-                            <button class="fetch-song">Fetch</button>
-                          </td>
-                        </tr>
-                      </table>
-                    </div>
-                  <?php } ?>
+        <?php for($i = 0; $i < 15; $i++) { ?>
+          <?php $song = $songs[$i]; ?>
+          <div class="song-field">
+            <div>
+              <label for="song-title-<?php echo $i;?>">
+                <?php _e('Title:','sm');?>
+              </label>
+              <input class="song-title" type="text" name="song-title-<?php echo $i;?>" id="song-title-<?php echo $i;?>" value="<?php echo stripslashes($song['title']); ?>" size="75"/>
+            </div>
+            
+            <div>
+              <label for="song-path-<?php echo $i;?>">
+                <?php _e('URL:','sm');?>
+              </label>
+              <input class="song-path" type="text" name="song-path-<?php echo $i;?>" id="song-path-<?php echo $i;?>" value="<?php echo $song['path']; ?>" size="75"/>
+            </div>
+            <table class="inline-table">
+              <tr>
+                <td>
+                  <div>
+                    <label for="song-buy_link-<?php echo $i;?>">
+                      <?php _e('Buy Link:','sm');?>
+                    </label>
+                    <input class="song-buy_link" type="text" name="song-buy_link-<?php echo $i;?>" id="song-buy_link-<?php echo $i;?>" value="<?php echo $song['buy_link']; ?>" size="75"/>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <label for="song-album_cover-<?php echo $i;?>">
+                      <?php _e('Album Cover:','sm');?>
+                    </label>
+                    <input class="song-album_cover" type="text" name="song-album_cover-<?php echo $i;?>" id="song-album_cover-<?php echo $i;?>" value="<?php echo $song['album_cover']; ?>" size="75"/>
+                  </div>
+                </td>
+                <td class="album_cover_preview"><div class="preview_wrapper"><img src="<?php echo $song['album_cover']; ?>"/></div></td>
+                <td>
+                  <div class="song-checkbox">
+                    <input class="song-show_download" type="checkbox" name="song-show_download-<?php echo $i;?>" id="song-show_download-<?php echo $i;?>" <?php if($song['show_download'] == true) echo "checked='checked'"; ?> />&nbsp;<label for="song-show-download-<?php echo $i;?>"><?php _e('Show&nbsp;Download','sm');?></label>
+                  </div>
+                </td>
+                <td>
+                  <button class="fetch-song">Fetch</button>
+                </td>
+              </tr>
+            </table>
+          </div>
+        <?php } ?>
                              
-                </fieldset>
-              </td>
-            </tr>
-                 
-          </tbody>
-        </table>
         <input type="hidden" name="action" value="update" />                    
         <p class="submit">
           <input type="submit" name="updateoptions" value="<?php _e('Save Changes') ?>" />
@@ -362,7 +346,7 @@ function sm_pageOptions() {
  */
 function sm_printAdminFooter() {
 ?>
-  <p class="footer_st" style="font-size:0.8em;text-align:center;"><?php printf(__('&copy; Copyright 2008 <a href="http://www.charlestang.cn/" title="Here With Me">Charles Tang</a> | <a href="http://www.charlestang.cn/flash-mp3-player.htm">Flash MP3 Player</a> | Version %s', 'sm'), '8.8.7'); ?></p>
+  <p class="footer_st" style="font-size:0.8em;text-align:center;"><?php printf(__('&copy; Copyright 2008 <a href="http://www.charlestang.cn/" title="Here With Me">Charles Tang</a> | <a href="http://www.charlestang.cn/flash-mp3-player.htm">SoundMachine</a> | Version %s', 'sm'), '8.8.7'); ?></p>
   <?php
 } 
 
